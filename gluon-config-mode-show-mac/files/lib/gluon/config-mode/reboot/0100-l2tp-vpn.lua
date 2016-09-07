bd5a1de67a144cb279bc1a8f542fab64d2899568
HellMar <github@hellmann.me>
2016-09-07 12:55:43 +0200

    Initial commit gluon-config-mode-show-mac

---
+++ gluon-config-mode-show-mac/files/lib/gluon/config-mode/reboot/0100-l2tp-vpn.lua
@@ -0,0 +1,21 @@
+local uci = luci.model.uci.cursor()
+
+local meshvpn_enabled = uci:get('tunneldigger', uci:get_first('tunneldigger', 'broker'), 'enabled')
+
+local i18n = require 'luci.i18n'
+local util = require 'luci.util'
+local site = require 'gluon.site_config'
+local sysconfig = require 'gluon.sysconfig'
+
+local macaddr = uci:get('network', 'bat0', 'macaddr')
+local hostname = uci:get_first("system", "system", "hostname")
+local msg = i18n.translate('gluon-config-mode:macaddr')
+
+return function()
+    luci.template.render_string(msg, {
+        macaddr = macaddr,
+        hostname = hostname,
+        site = site,
+        sysconfig = sysconfig
+    })
+end
