3039f7dee5248e10e16141cbecbaf0c25b7158e9
RobWei <robwei280101@gmail.com>
2018-06-14 22:30:41 +0200

    * fixed syntax

--- gluon-config-mode-show-mac/files/lib/gluon/config-mode/reboot/0100-l2tp-vpn.lua
+++ gluon-config-mode-show-mac/files/lib/gluon/config-mode/reboot/0100-l2tp-vpn.lua
@@ -1,13 +1,15 @@
+local site_i18n = i18n 'gluon-site'
 local uci = require("simple-uci").cursor()
-local lutil = require 'gluon.web.util'
-local util = require 'gluon.util'
+
 local site = require 'gluon.site_config'
 local sysconfig = require 'gluon.sysconfig'
 local pretty_hostname = require 'pretty_hostname'
 
-local macaddr = uci:get_first('tunneldigger', 'broker', 'uuid')
+local macaddr = uci:get_first('tunneldigger', 'mesh_vpn', 'uuid')
 local hostname = pretty_hostname.get(uci)
-local msg = _translate('gluon-config-mode:macaddr')
+
+local msg = site_i18n._translate('gluon-config-mode:macaddr')
+if not msg then return end
 
 renderer.render_string(msg, {
    macaddr = macaddr,
