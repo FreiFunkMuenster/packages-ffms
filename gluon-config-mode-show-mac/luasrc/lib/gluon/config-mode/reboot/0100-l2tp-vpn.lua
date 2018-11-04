e17aa0c497360e24c07a6cbd25374fea5c929a30
RobWei <admin@robwei.me>
2018-11-04 20:08:27 +0100

    * update to v2018.1.x

--- gluon-config-mode-show-mac/luasrc/lib/gluon/config-mode/reboot/0100-l2tp-vpn.lua
+++ gluon-config-mode-show-mac/luasrc/lib/gluon/config-mode/reboot/0100-l2tp-vpn.lua
@@ -5,7 +5,7 @@ local site = require 'gluon.site_config'
 local sysconfig = require 'gluon.sysconfig'
 local pretty_hostname = require 'pretty_hostname'
 
-local macaddr = uci:get_first('tunneldigger', 'mesh_vpn', 'uuid')
+local macaddr = uci:get_first('tunneldigger', 'broker', 'uuid')
 local hostname = pretty_hostname.get(uci)
 
 local msg = site_i18n._translate('gluon-config-mode:macaddr')
