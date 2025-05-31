3e1cb97892e018ad99b6f6ec23d7e2e92cae9aa5
RobWei <admin@robwei.me>
2025-05-31 22:22:57 +0200

    [2021.1.2] check-site gefixt

--- ffmsl-eol-ssid/check_site.lua
+++ ffmsl-eol-ssid/check_site.lua
@@ -1,5 +1,5 @@
 obsolete({'eol_wifi_ssid'}, 'Use eol_ssid.ssid instead.')
 if need_boolean({'eol_ssid', 'enabled'}, false) then
-	need_boolean({'eol_ssid', 'as_additional_ssid', true})
+	need_boolean({'eol_ssid', 'as_additional_ssid'}, true)
 	need_string({'eol_ssid', 'ssid'})
 end
